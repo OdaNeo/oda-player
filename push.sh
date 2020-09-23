9b3a128cdea3a54b0e0a006ea836c3a2abb9849a
Oda-T <46777459+Oda-T@users.noreply.github.com>
2020-09-23 22:55:14 +0800

    add firebase

--- push.sh
+++ push.sh
@@ -2,9 +2,8 @@ set -e
 
 git add -A
 
-git commit -m 'remove postcss-config-js'
+git commit -m 'add firebase'
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
 git push git@github.com:Oda-T/OPlayer.git master:master
 
 cd -
